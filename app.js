28ff81c19cc8afb5c1d6a82b293046e83cd8183f
Sarbstore <146440379+Sarbstore@users.noreply.github.com>
2023-10-01 20:50:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function sideimg1function(){
+    document.getElementById("main").src = "71XkR1eC+7L._SX466_.jpg";
+    document.getElementById("main").style.transitionDuration = "1s";
+}
+function sideimg2function(){
+    document.getElementById("main").src = "71ZMB2eAPpL._SL1500_.jpg";
+}
+function sideimg3function(){
+    document.getElementById("main").src = "af6d6493-42d4-4bfc-b0d4-b4683d815631.__CR0,0,970,600_PT0_SX970_V1___.jpeg";
+}
+function sideimg4function(){
+    document.getElementById("main").src = "71g-seXJWsL._SL1300_.jpg";
+}
